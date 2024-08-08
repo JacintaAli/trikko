cbe7428cc9f65a806d86940e2d912224d8de6d6d
Jacinta Ali <jacintaali35@gmail.com>
2024-08-08 16:35:49 +0100

    Feat(trikko): Create the trikko e-commerce website. Incorporate redux for state management; handle the cart logic and handle the routing using react-router-dom

---
+++ src/data/reviewData.jsx
@@ -0,0 +1,36 @@
+import { bag_brickred, bag_brown, bag_girlie, bag_louis, bag_mainbg, bag_white } from "../assets/images";
+
+export const reviewData = [
+    {
+        id: "82a0xa280",
+        name: "Angela Monroe",
+        image: bag_girlie,
+        rating: 5,
+        productImage: bag_brickred,
+        text: "The article element represents a complete, or self-contained, composition in a document, page, application, or site and that is in principle."
+    },
+    {
+        id: "82a0xa281",
+        name: "Jacinta Jackson",
+        image: bag_mainbg,
+        rating: 3,
+       // productImage: bag_white,
+        text: "The bag i bought was incredible but overpriced, composition in a document, page, application, or site and that is in principle."
+    },
+    {
+        id: "82a0xa282",
+        name: "Lee Dong Wook",
+        image: bag_brown,
+        rating: 4,
+       // productImage: bag_white,
+        text: "Trikko products have been a source of flair for me and my kids and my wife, and that is in principle."
+    },
+    {
+        id: "82a0xa280",
+        name: "Keith Powers",
+        image: bag_louis,
+        rating: 5,
+        productImage: bag_white,
+        text: "I have been hooked by Trikko ever since my first purchase, I do not feel the need to ever change my deal,and that is in principle."
+    }
+]
